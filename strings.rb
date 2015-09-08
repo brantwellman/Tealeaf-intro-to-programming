0eb2772f3b0402654637aba0ac111746bd2f6d9b
Brant Wellman <BrantWellman@gmail.com>
2015-09-08 15:10:48 -0700

    Strings complete

---
+++ strings.rb
@@ -0,0 +1,39 @@
+# string - 1
+puts "Brant" + " Wellman"
+
+# operators - 2
+puts "number is 4567"
+#thousands
+puts 4567 / 1000
+#hundreds
+puts 4567 % 1000 / 100
+#tens
+puts 4567 % 100 / 10
+# ones
+puts 4567 % 10
+
+#hash - 3
+movies = { movie1: 1975, movie2: 2004, movie3: 2013, movie4: 2001, movie5: 1981 }
+puts movies[:movie1]
+puts movies[:movie2]
+puts movies[:movie3]
+puts movies[:movie4]
+puts movies[:movie5]
+
+#arrays - 4
+movies = [1975, 2004, 2013, 2001, 1981]
+puts movies
+
+#factorial - 5
+puts 5*4*3*2*1
+puts 6*5*4*3*2*1
+puts 7*6*5*4*3*2*1
+puts 8*7*6*5*4*3*2*1
+
+#squares - floats - 6
+puts 4.3*4.3
+puts 2.2*2.2
+puts 3.56*3.56
+
+#error message - 7
+# There is a opening brace - { - somewhere in the code without a corresponding closing brace - }
